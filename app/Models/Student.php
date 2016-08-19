8423e6ce59b9f6849f910cbf04f88481ed87fe8c
Hafiz Junaid Shahid <hafizjunaidshahid@gmail.com>
2016-08-19 08:52:07 +0500

    database issue

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -16,34 +16,15 @@ class Student extends Schooledge  {
 
 	public static function querySelect(  ){
 		
-		return "  SELECT tb_user.* FROM tb_user  ";
+		return "  SELECT tb_users.* FROM tb_users  ";
 	}	
 
 	public static function queryWhere(  ){
 		
-		return "  WHERE tb_user.id IS NOT NULL ";
+		return "  WHERE tb_users.id IS NOT NULL ";
 	}
 	
 	public static function queryGroup(){
 		return "  ";
 	}
-
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token'
-    ];
 }
