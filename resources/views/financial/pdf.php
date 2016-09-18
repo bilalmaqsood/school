9a5a062d07215853ef5b139f9176c5042ce4163e
Hafiz Junaid Shahid <hafizjunaidshahid@gmail.com>
2016-09-18 20:37:46 +0500

    fix design issue in receipt

--- resources/views/financial/pdf.php
+++ resources/views/financial/pdf.php
@@ -39,10 +39,10 @@
     <p class="MsoNoSpacing" align="center" style="text-align:center;"><strong><span style="font-size:15.0pt; ">                                           </span></strong><strong><span style="font-family:Aharoni; font-size:25.0pt; ">RECEIPT</span></strong><span style="font-family:Aharoni; font-size:25.0pt; ">  </span><span style="font-size:25.0pt; ">  </span><span style="font-size:26.0pt; ">                   </span><strong><span style="font-size:14.0pt; ">No</span></strong>.<?php echo $no; ?></p>
     <blockquote>
         <p class="MsoNoSpacing" style="line-height:150%;"><span style="line-height:150%; font-family:'Times New Roman','serif'; font-size:12.0pt; "> Date: <?php echo date(CNF_DATEFORMAT, strtotime($received_date)); ?></span></p>
-        <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; ">Received from _________________<?php echo ucwords($student_id); ?>______________</span></p>
-        <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; "> The sum of__________<?php echo ucwords($amount_in_words); ?>____________ dollars</span></p>
-        <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; "> For_________<?php echo ucwords($purpose); ?>___________<br>
-    Amount  Paid <?php echo CNF_CURRENCY.' _______'.$amount.'_______'; ?>   
+        <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; ">Received from:</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;             <?php echo ucwords($student_id); ?></p>
+        <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; "> The sum of&nbsp;<?php echo strtolower($amount_in_words); ?>&nbsp;dollars</span></p>
+        <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; "> For&nbsp;<?php echo strtolower($purpose);?><br>
+    Amount  Paid <?php echo CNF_CURRENCY.' '.$amount.' '; ?>   
         <?php if($due = 0) { ?>
             <p class="MsoNoSpacing" style="line-height:150%;"><span style="font-family:'Times New Roman','serif'; font-size:12.0pt; "> Balance  Due <?php echo CNF_CURRENCY.' _______'.$due.'_______'; ?>                                       </span></p>
         <?php } ?>
